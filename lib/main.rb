39cb163c5b80bb8123be0373e2e913310ab68222
Alejandro Contreras <kdta15@outlook.com>
2021-02-03 18:28:29 -0600

    Added the rspec tests

--- lib/main.rb
+++ lib/main.rb
@@ -1,29 +1,41 @@
+# frozen_string_literal: true
+
+# Assigns the reply varible its value depending on the message
 class Answer
-  attr_accessor :reply
+  attr_reader :reply, :simple_answer, :option_answer
 
   def initialize
     @reply = ''
-  end
-
-  def ans_img(message)
     @simple_answer = ['../img/yes.jpg', '../img/no.jpg', '../img/dont_think.jpg', '../img/definitely.jpg',
                       '../img/maybe.jpg', '../img/never.jpg'].sample
     @option_answer = ['../img/neither.jpg', '../img/nothing.jpg', '../img/first.jpg', '../img/second.jpg'].sample
+  end
+
+  def question(message)
     @reply = case message.downcase
-             when %r{/start}
-               '../img/start.jpg'
-             when /bye|goodbye|thanks/
-               '../img/end.jpg'
              when /\bor\b/
                @option_answer
              when /\sdo\s/
                '../img/nothing.jpg'
              when /else\sbut\sno|when\s|how/
                '../img/try_again.jpg'
-             when /\?/
+             else
                @simple_answer
+             end
+  end
+
+  def no_question(message)
+    @reply = case message.downcase
+             when %r{/start}
+               '../img/start.jpg'
+             when /bye|goodbye|thanks/
+               '../img/end.jpg'
              else
                '../img/try_again.jpg'
              end
+  end
+
+  def ans_img(message)
+    message[-1, 1] == '?' ? question(message) : no_question(message)
   end
 end
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -6,10 +6,59 @@ require_relative '../lib/main'
 
 describe Answer do
   describe '.ans_img' do
-  let(:ans_test) {Answer.new}
-    it 'The reply is the expected due the message' do
-      ans_test.ans_img('When did he leave?')
-      expect(ans_test.reply).to eql('../img/try_again.jpg')
+    let(:ans_test) { Answer.new }
+    context 'Will return the expected image depending on the message' do
+      it 'returns a random image from the variable simple_answer' do
+        ans_test.ans_img('will I ever get married?')
+        expect(ans_test.reply).to eql(ans_test.simple_answer)
+      end
+
+      it 'returns a random image from the variable simple_answer no matter if its lowercase or uppercase' do
+        ans_test.ans_img('DO YOU LIKE MY SHOES?')
+        expect(ans_test.reply).to eql(ans_test.simple_answer)
+      end
+
+      it 'returns a random image from the variable simple_answer' do
+        ans_test.ans_img('should i drink water or coffee?')
+        expect(ans_test.reply).to eql(ans_test.option_answer)
+      end
+
+      it 'returns the specific image when the message is bye, goodbye or thanks' do
+        ans_test.ans_img('should i drink water or coffee?')
+        expect(ans_test.reply).to eql(ans_test.option_answer)
+      end
+
+      it 'returns nothing if the question has the word DO' do
+        ans_test.ans_img('what do we need to do?')
+        expect(ans_test.reply).to eql('../img/nothing.jpg')
+      end
+    end
+  end
+end
+
+describe Answer do
+  describe '.ans_img' do
+    let(:ans_test) { Answer.new }
+    context "It will return the img 'try again' when the expected messaged is input (BAD USAGE)" do
+      it "the word 'When' triggers this" do
+        ans_test.ans_img('When did he leave?')
+        expect(ans_test.reply).to eql('../img/try_again.jpg')
+      end
+
+      it "the word 'How' triggers this" do
+        ans_test.ans_img('How are you?')
+        expect(ans_test.reply).to eql('../img/try_again.jpg')
+      end
+
+      it 'When the message is not a question' do
+        ans_test.ans_img('This is not a question')
+        expect(ans_test.reply).to eql('../img/try_again.jpg')
+      end
+
+      it 'With this special little easter egg' do
+        ans_test.ans_img("Can't you say anything else but no?")
+        expect(ans_test.reply).to eql('../img/try_again.jpg')
+      end
     end
   end
 end
